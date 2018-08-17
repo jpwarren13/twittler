7af8ad630d1955354d1f671647d26a418b41890a
johnpwarren@gmail.com <johnpwarren@gmail.com>
2018-08-17 15:38:22 -0700

    * Changed UI to look how I want it to be.

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
       $(document).ready(function(){
         var $body = $('body');
+
        // $body.html('');
 
           for(var i = 0; i < streams.home.length; i++){
+          var $tweetBody=$('<div class="tweetBody"></div>');
           var tweet = streams.home[i];
+          var $user = $('<div class="user"></div>');
           var $tweet = $('<div class="tweet"></div>');
-          $tweet.text('@' + tweet.user + ': ' + tweet.message);
-          $tweet.appendTo($body);
+
+          $user.text('@' + tweet.user);
+          $tweet.text(tweet.message);
+
+          //$tweetBody.append($user);
+          $user.appendTo($tweetBody);
+          $tweet.appendTo($tweetBody);
+          $tweetBody.appendTo($body);
         }
         setInterval(tweetStreet, 3000);
       });
